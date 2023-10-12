6cf6ff14108f6a598c4c5f19db2d193a5de4d858
Programmer310 <89231213+Programmer310@users.noreply.github.com>
2023-10-11 17:39:09 -0700

    Completed Ch.21. Added feature that allows users to search through Flickr images and save query results when leaving the app.

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -1,17 +1,13 @@
 package com.bignerdranch.android.photogallery.api
 
 import retrofit2.http.GET
-
-private const val API_KEY = "8fa20205240fd964d8b0e50a4107b53f"
+import retrofit2.http.Query
 
 interface FlickrApi {
 
-    @GET(
-        "services/rest/?method=flickr.interestingness.getList" +
-        "&api_key=$API_KEY" +
-        "&format=json" +
-        "&nojsoncallback=1" +
-        "&extras=url_s"
-    )
+    @GET("services/rest/?method=flickr.interestingness.getList")
     suspend fun fetchPhotos(): FlickrResponse
+
+    @GET("services/rest?method=flickr.photos.search")
+    suspend fun searchPhotos(@Query("text") query: String): FlickrResponse
 }
